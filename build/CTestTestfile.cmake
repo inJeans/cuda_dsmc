cf957290907edb12734e90f8f340d82081dff680
Chris Watkins (macbook) <christopher.watkins@me.com>
2015-09-29 22:08:19 +1000

    Removed build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/miMacbookPro/Documents/GitMercurial/cuda_dsmc
-# Build directory: /Users/miMacbookPro/Documents/GitMercurial/cuda_dsmc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(cuda_dmsc_runs "cuda_dsmc")
