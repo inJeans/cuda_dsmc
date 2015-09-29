643073420de7f324c164e52e5703384caec88412
Chris Watkins (macbook) <christopher.watkins@me.com>
2015-09-29 22:07:50 +1000

    Added build directories to repo.

---
+++ build/CMakeFiles/cuda_dsmc.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/miMacbookPro/Documents/GitMercurial/cuda_dsmc/src/main.c" "/Users/miMacbookPro/Documents/GitMercurial/cuda_dsmc/build/CMakeFiles/cuda_dsmc.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/Developer/NVIDIA/CUDA-7.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/cuda_dsmc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_dsmc.dir/src/main.c.o"
+  "cuda_dsmc.pdb"
+  "cuda_dsmc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cuda_dsmc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
