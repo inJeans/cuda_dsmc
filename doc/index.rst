993856ab8ae2b364ff7bdce95f9967fe695e33eb
Chris Watkins (macbook) <christopher.watkins@me.com>
2015-10-10 23:11:24 +1100

    Setting up doxygen -> sphinx -> rtd

---
+++ doc/index.rst
@@ -0,0 +1,25 @@
+.. CUDA DSMC documentation master file, created by
+   sphinx-quickstart on Sat Oct 10 22:36:55 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to CUDA DSMC's documentation!
+=====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+.. doxygenfile:: main.c
+    :project: CUDA_DSMC
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+// @file
+// file description
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
@@ -8,7 +11,20 @@ int main(int argc, char const *argv[])
 	printf( "*                          *\n" );
 	printf( "****************************\n" );
 
-	// Generate distribution
+	// Generate distribution.
 
 	return 0;
+}
+
+/** \fn void doxygen_test( double x )
+ *  \brief Short description
+ *  \param x double that gets printed
+ *  \warning What does this do?
+ *  Detailed description starts here.
+ */
+
+void doxygen_test( double x )
+{
+	printf("%f\n", x);
+	return;
 }
