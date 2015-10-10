bbdde2e42e9eb9042fc31fd684c6a3d41a850547
Chris Watkins (macbook) <christopher.watkins@me.com>
2015-10-10 23:27:52 +1100

    Fixed typo

--- doc/conf.py
+++ doc/conf.py
@@ -21,8 +21,8 @@ import subprocess, os
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-    
-    subprocess.call('cd ../doxygen; doxygen doxygen.config', shell=True)
+
+    subprocess.call('cd ../doxygen; doxygen doxyfile.config', shell=True)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
