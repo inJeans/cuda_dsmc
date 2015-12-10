71d070919482fa1e26cf0d681ded2c7c7f532ce5
Watkins, Chris (IM&T, Clayton) <Chris.Watkins@csiro.au>
2015-12-10 21:26:02 +1100

    Removed some more cuda header dependencies.

--- include/cuda_dsmc/distribution_generation.hpp
+++ include/cuda_dsmc/distribution_generation.hpp
@@ -1,9 +1,12 @@
-
+#ifdef CUDA
 #include <cuda_runtime.h>
+#endif
 
 #include "random_numbers.hpp"
 
+#ifdef CUDA
 __host__ void generate_thermal_velocities(int num_atoms,
                                           double temp,
                                           curandState *state,
                                           double3 *vel);
+#endif
