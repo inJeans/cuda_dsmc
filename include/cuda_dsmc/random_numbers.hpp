bd0c82810fccf78322f9cbf4bd60ea9596b0415e
Watkins, Chris (IM&T, Clayton) <Chris.Watkins@csiro.au>
2015-12-10 21:09:19 +1100

    Updated randon_numbers.hpp to avoid using cuda when on windows.

--- include/cuda_dsmc/random_numbers.hpp
+++ include/cuda_dsmc/random_numbers.hpp
@@ -11,17 +11,21 @@
 #include "entropy.h"
 
 #include <math.h>
+#ifdef CUDA
 #include <cuda_runtime.h>
 #include <cuda.h>
 #include <curand_kernel.h>
+#endif
 
 #include <iostream>
 
 // __host__ double h_gaussian_ziggurat(pcg32_ranm,dom_t *seed);
 // __host__ double uniform_prng(pcg32_random_t *seed);
 
+#ifdef CUDA
 void initialise_rng_states(int n_states,
                            curandState *state);
+#endif
 
 void initialise_rng_states(int n_states,
                            pcg64_random_t *state,
