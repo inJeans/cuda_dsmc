4a5b59964dd91bded977feef06cd1c65221bc700
Watkins, Chris (IM&T, Clayton) <Chris.Watkins@csiro.au>
2016-03-24 08:39:20 +1100

    Extending simulation time to try an get some collisions.

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -13,19 +13,19 @@ void progress_bar(int current,
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
 
-    int hashes_needed = current * (w.ws_col-6) / total + 1;
+    int hashes_needed = current * (w.ws_col-7) / total + 1;
 
     if (current == 0) printf("\n");
 
-    for (int i = 0; i < hashes_needed+6; ++i) printf("\b");
     if (current * 100 / total < 35)
-        printf("\033[1;31m%d%%|", current * 100 / total + 1);
+        printf("\033[31m %3d%%|", current * 100 / total + 1);
     else if (current * 100 / total < 70)
-        printf("\033[1;33m%d%%|", current * 100 / total + 1);
+        printf("\033[33m %3d%%|", current * 100 / total + 1);
     else
-        printf("\033[1;32m%d%%|", current * 100 / total + 1);
+        printf("\033[32m %3d%%|", current * 100 / total + 1);
     for (int i = 0; i < hashes_needed; ++i) printf("#");
-    printf("|");
+    printf("|\033[m");
+    for (int i = 0; i < hashes_needed+7; ++i) printf("\b");
     if (current == total) printf("\n");
     fflush(stdout);
 
