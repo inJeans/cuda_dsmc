0a84e30eb34f4124c7c6e49e31877547666373d6
Chris Watkins (macbook) <christopher.watkins@me.com>
2017-02-10 05:06:10 +1100

    Minimal compiling code in a container.

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/** \file
+ *  \brief Main code
+ *
+ *  More detailed description
+ *  Copyright 2017 Christopher Watkins
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    printf("****************************\n");
+    printf("*                          *\n");
+    printf("*   WELCOME TO CUDA DSMC   *\n");
+    printf("*                          *\n");
+    printf("****************************\n");
+
+    return 0;
+}
