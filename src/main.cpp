b2ad153f66afa6f77deecb5d1dfc5ef47f0e9280
Chris Watkins (macbook) <christopher.watkins@me.com>
2017-03-22 05:18:31 +1100

    Added a minimal logger to the main code.

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,38 @@
  */
 
 #include <stdio.h>
+#include <g3log/g3log.hpp>
+#include <g3log/logworker.hpp>
+#include <g3log/std2_make_unique.hpp>
+
+#include <string>
+
+#include "CustomSink.hpp"
+
+#if (defined(WIN32) || defined(_WIN32) || defined(__WIN32__))
+    mkdir("./tmp", 0700);
+    const std::string kPathToLogFile = "./tmp/";
+#else
+    const std::string kPathToLogFile = "/tmp/";
+#endif
+const std::string kLogfilename = "cuda_dsmc";
 
 int main(int argc, char const *argv[]) {
-    printf("****************************\n");
-    printf("*                          *\n");
-    printf("*   WELCOME TO CUDA DSMC   *\n");
-    printf("*                          *\n");
-    printf("****************************\n");
+    auto worker = g3::LogWorker::createLogWorker();
+    auto logfileHandle = worker->addDefaultLogger(kLogfilename,
+                                                  kPathToLogFile);
+
+    // logger is initialized
+    g3::initializeLogging(worker.get());
+
+    auto stdoutHandle = worker->addSink(std2::make_unique<CustomSink>(),
+                                        &CustomSink::ReceiveLogMessage);
+
+    LOGF(INFO, "****************************");
+    LOGF(INFO, "*                          *");
+    LOGF(INFO, "*   WELCOME TO CUDA DSMC   *");
+    LOGF(INFO, "*                          *");
+    LOGF(INFO, "****************************");
 
     printf("PASSED\n");
 
